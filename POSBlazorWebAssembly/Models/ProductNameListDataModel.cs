00afc2a66179c747e4b20657171b831838f59f9a
NayPaingSoe <naypaingsoe042@gmail.com>
2026-05-18 16:01:25 +0630

    Implement Product Sale functionality and related models

---
+++ POSBlazorWebAssembly/Models/ProductNameListDataModel.cs
@@ -0,0 +1,8 @@
+namespace POSBlazorWebAssembly.Models
+{
+    public class ProductNameListDataModel
+    {
+        public Guid? product_id { get; set; }
+        public string product_name { get; set;}
+    }
+}
